fcca965fbfd33d47daa360ba94c5b64586faac6e
Sebastian <h_p_r@web.de>
2021-12-19 14:55:23 +0100

    Update ui to include months, update buttons to show only dates til today

--- commit_using_ui.py
+++ commit_using_ui.py
@@ -14,54 +14,74 @@ class PushButton(QPushButton):
         self.setText(text)
         self.setMaximumSize(QSize(20, 20))
         self.setContentsMargins(10,10,10,10)
-        
+
+class Label(QLabel):
+    def __init__(self, text, parent=None):
+        super(Label, self).__init__(text, parent)
+
+        self.text = text
+
+        self.setText(text)
+        self.setMaximumSize(QSize(20, 20))
+        self.setContentsMargins(10,10,10,10)
+
 
 class MyWindow(QMainWindow):
     def __init__(self):
         super(MyWindow, self).__init__()
 
-        self.days = 8
-        self.weeks = 53
+        self.rows = 9
+        self.cols = 53
 
         self.selected_days = []
-        
+
         centralWidget = QWidget()
         self.setCentralWidget(centralWidget)
+        centralWidget.setStyleSheet(f"""background-color: lightgrey""")
 
         self.layout = QGridLayout(centralWidget)
 
-        for row in range(1, self.days): 
+        today = datetime.today()
+        my_date = datetime.strptime(str.rstrip("2020-12-20"), '%Y-%m-%d')
+        month = my_date.strftime('%b')
+        label = Label(f'{month}')
+        self.layout.addWidget(label, 0, 0)
+
+        for row in range(2, self.days): 
            for column in range(0, self.weeks):
-                buttonname = row + column * 7
-                button = PushButton(f'{buttonname}', self)
-                button.clicked.connect(partial(self.onClicked, buttonname, button))
-                button.setStyleSheet(f"""background-color: lightgrey""")
-                self.layout.addWidget(button, row, column)
-        
+                button_date = my_date + timedelta(row - 1 + column * 7 - 1)
+
+                if button_date <= today:
+                    button = PushButton(f'{button_date}', self)
+                    button.clicked.connect(partial(self.onClicked, button_date, button))
+                    button.setStyleSheet(f"""background-color: #ebedf0""")
+                    self.layout.addWidget(button, row, column)
+
+                    if button_date.strftime('%b') != month and row == 2:
+                        month = button_date.strftime('%b')
+                        label = Label(f'{month}')
+                        self.layout.addWidget(label, 0, column)
+
         buttonname = 'push'
         button = PushButton(f'{buttonname}', self)
         button.clicked.connect(partial(self.onClickedSend))
-        button.setStyleSheet(f"""background-color: lightgrey""")
+        button.setStyleSheet(f"""background-color: #ebedf0""")
         self.layout.addWidget(button, 8, 0, 1, 2)
 
     def checkForDateInList(self, date):
         if str(date) in self.selected_days:
             self.selected_days.remove(str(date))
-            colorReturn = 'lightgrey'
+            colorReturn = '#ebedf0'
         else:
             self.selected_days.append(str(date))
-            colorReturn = 'green'
+            colorReturn = '#40c463'
 
         return colorReturn
 
     def onClicked(self, name, button):
-        my_date = datetime.strptime(str.rstrip("2020-12-20"), '%Y-%m-%d')
-        selected_date = my_date + timedelta(days=name-1)
-        print(selected_date)
-        color = self.checkForDateInList(selected_date)
+        print(name)
+        color = self.checkForDateInList(name)
         button.setStyleSheet(f"""background-color: {color}""")
-
-
 
     def onClickedSend(self):
         self.selected_days.sort()
@@ -75,8 +95,7 @@ class MyWindow(QMainWindow):
             commitMessage = f"""git commit -m "Add date {day} to README" --date="{day}" """
             os.system(commitMessage)
         os.system("git push")
-        
-                
+
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     w = MyWindow()
